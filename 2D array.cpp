d950f921d78f1049ee3002bc131bb80c08238c00
Madhav Gupta <80634128+mad18-lab@users.noreply.github.com>
2023-06-10 14:20:34 +0530

    Add files via upload

---
+++ 2D array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cout<<"\nEnter number of rows: ";
+    cin>>r;
+    cout<<"\nEnter number of columns: ";
+    cin>>c;
+    int arr[r][c];
+    
+    cout<<"\nEnter elements of 2D array: ";
+    for (int i=0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin>>arr[i][j];
+        }
+    }
+    
+    cout<<"\nOriginal Array: ";
+    for (int i = 0; i < r; i++) {
+        cout<<endl;
+        for (int j = 0; j < c; j++) {
+            cout<<arr[i][j]<<" ";
+        }
+    }
+}
