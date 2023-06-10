88fbaedb6d74dc28866fde983b38cde6b13a0c6c
Madhav Gupta <80634128+mad18-lab@users.noreply.github.com>
2023-06-10 21:44:50 +0530

    Add files via upload

---
+++ sorting a string alphabetically and case-wise.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str = "my name is madhav Gupta";
+    
+    sort(str.begin(), str.end());
+    
+    cout<<str;
+    
+    return 0;
+}
